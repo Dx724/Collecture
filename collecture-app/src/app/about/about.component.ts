c25cb7f35a669f1914ea56c1187ed1a54de052c7
Dx724 <dx72dx@gmail.com>
2020-04-17 20:12:43 -0400

    Material-design search bar

--- collecture-app/src/app/about/about.component.ts
+++ collecture-app/src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LectureService } from '../lecture.service';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private lectureService: LectureService) { }
 
   ngOnInit(): void {
+    this.lectureService.setActiveTitle("");
   }
 
 }
--- collecture-app/src/app/app.module.ts
+++ collecture-app/src/app/app.module.ts
@@ -11,7 +11,9 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatDividerModule, MatDivider } from '@angular/material/divider';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { LayoutModule } from '@angular/cdk/layout';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -40,7 +42,9 @@ import { AboutComponent } from './about/about.component';
     LayoutModule,
     MatToolbarModule,
     HttpClientModule,
-    MatDividerModule
+    MatDividerModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
